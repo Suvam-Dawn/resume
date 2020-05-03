385094a6b4ba26fd8271daac8d7220c9cb222fef
Suvam <suvamdawn94@gmail.com>
2020-05-03 21:25:45 +0530

    Side menu component created and move side menu contents

--- src/components/General.js
+++ src/components/General.js
@@ -1,13 +1,8 @@
 import React, {Component} from 'react';
 import {Row, Col} from 'react-bootstrap';
-import {FiMenu} from 'react-icons/fi';
-import {MdWork, MdSettings, MdContacts} from 'react-icons/md';
-import {FaUserAlt, FaUserGraduate} from 'react-icons/fa';
-import {AiOutlineCloudDownload} from 'react-icons/ai';
 import Content from './Content';
 import {Helmet} from 'react-helmet';
-import ReactDOM from 'react-dom';
-import {Link} from 'react-router-dom';
+import SideMenu from './SideMenu';
 export default class General extends Component {
   constructor(props) {
     super(props);
@@ -78,33 +73,7 @@ export default class General extends Component {
               xs={6}
               className={'p-0 content_div_left'}
             >
-              <div className={'content_div__head'}>
-                <div className="left-menu">
-                  <div className={'menuBtn'}>
-                    <FiMenu size={25} />
-                  </div>
-                  <div className={'menuList'}>
-                    <Link to="/about">
-                      <FaUserAlt size={25} />
-                    </Link>
-                    <Link to="/experience">
-                      <MdWork size={25} />
-                    </Link>
-                    <Link to="/skills">
-                      <MdSettings size={25} />
-                    </Link>
-                    <Link to="/education">
-                      <FaUserGraduate size={25} />
-                    </Link>
-                    <Link to="/contact">
-                      <MdContacts size={25} />
-                    </Link>
-                  </div>
-                  <div className={'download'}>
-                    <AiOutlineCloudDownload size={45} />
-                  </div>
-                </div>
-              </div>
+              <SideMenu />
             </Col>
             <Col xl={6} lg={6} md={6} sm={6} xs={6}>
               <Content />
---
+++ src/components/SideMenu.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {FiMenu} from 'react-icons/fi';
+import {MdWork, MdSettings, MdContacts} from 'react-icons/md';
+import {FaUserAlt, FaUserGraduate} from 'react-icons/fa';
+import {AiOutlineCloudDownload} from 'react-icons/ai';
+import {Link} from 'react-router-dom';
+export default class General extends Component {
+  render() {
+    return (
+      <div className={'content_div__head'}>
+        <div className="left-menu">
+          <div className={'menuBtn'}>
+            <FiMenu size={25} />
+          </div>
+          <div className={'menuList'}>
+            <Link to="/about">
+              <FaUserAlt size={25} />
+            </Link>
+            <Link to="/experience">
+              <MdWork size={25} />
+            </Link>
+            <Link to="/skills">
+              <MdSettings size={25} />
+            </Link>
+            <Link to="/education">
+              <FaUserGraduate size={25} />
+            </Link>
+            <Link to="/contact">
+              <MdContacts size={25} />
+            </Link>
+          </div>
+          <div className={'download'}>
+            <AiOutlineCloudDownload size={45} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
