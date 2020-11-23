3d461e8d562409498f534a00f8c6363f565e2911
suvamdawn94 <suvamdawn94@gmail.com>
2020-11-23 18:32:03 +0530

    Updates

---
+++ precache-manifest.34b7152fdb9f3fcd8f53b3711471c86f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e158bb3e7f37ca28fd23090c6e2e107",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "fbf356a26aa05a2b8d68",
+    "url": "/my-resumestatic/css/2.539e65a7.chunk.css"
+  },
+  {
+    "revision": "29f0477dbf0215e131cc",
+    "url": "/my-resumestatic/css/main.7d11f3b3.chunk.css"
+  },
+  {
+    "revision": "fbf356a26aa05a2b8d68",
+    "url": "/my-resumestatic/js/2.97cdc38a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-resumestatic/js/2.97cdc38a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f0477dbf0215e131cc",
+    "url": "/my-resumestatic/js/main.325d1c3f.chunk.js"
+  },
+  {
+    "revision": "b291f8f186f827d50513",
+    "url": "/my-resumestatic/js/runtime-main.40b72ecf.js"
+  },
+  {
+    "revision": "fb5368b5e37d636e36a019e650a64832",
+    "url": "/my-resumestatic/media/Suvam_Dawn_Resume(7407620534_8436758250_suvamdawn94@gmail.com).fb5368b5.pdf"
+  },
+  {
+    "revision": "24c6d537a999d10e3facc5e26cd52f85",
+    "url": "/my-resumestatic/media/UNMATCHED_DEDICATION_AWARD.24c6d537.png"
+  }
+]);
