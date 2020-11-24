d643cda7aa4e9c3ac87ee63d575d0367dd0bcc85
suvamdawn94 <suvamdawn94@gmail.com>
2020-11-24 15:40:31 +0530

    image viewer added

--- src/components/info/Award.js
+++ src/components/info/Award.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactFancyBox from 'react-fancybox';
+import 'react-fancybox/lib/fancybox.css';
 export default class Award extends Component {
 	constructor(props) {
 		super(props);
@@ -20,11 +22,12 @@ export default class Award extends Component {
 				<h6 className={'experience_year'}>{awardResult.name + ' ( ' + awardResult.year + ' ) '}</h6>
 				<h6 className={'experience_company'}>{awardResult.company}</h6>
 				<h6 className={'experience_company_location'}>{awardResult.companyLocation}</h6>
-				<img
+				{/* <img
 					src={awardResult.image}
 					alt={awardResult.name}
 					style={{ width: '100%', paddingRight: '20px', maxHeight: '400px' }}
-				></img>
+				></img> */}
+				<ReactFancyBox thumbnail={awardResult.image} image={awardResult.image} />
 			</div>
 		);
 	};
